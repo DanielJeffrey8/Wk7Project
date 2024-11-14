d27688187b3e6d2c0f892a6cf9e4ce12ee7c871d
DanielJeffrey8 <DanielJeffrey8@hotmail.com>
2024-11-14 12:46:15 -0700

    first maven project

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,35 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import projects.exception.DbException;
+
+public class DbConnection 
+{
+	// CONSTANT VARIABLES
+	private static String     HOST = "localhost";
+	private static String PASSWORD = "project";
+	private static String   SCHEMA = "project";
+	private static String    USER  = "project";
+	private static    int     PORT =  3306;
+	
+	// METHODS
+	public static Connection getConnection()
+	{
+		String uri = String.format("jdbc:mysql://%s  :  %d  /   %s   ?  user = %s  &  password = %s  &  useSSL=false", 
+				                    HOST,  PORT, SCHEMA, USER,  PASSWORD );
+		System.out.println("Connection with uri = " + uri);
+		
+		try {
+			Connection conn = DriverManager.getConnection(uri);
+			System.out.println("Successfull Connection!");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("Unable to get connection at " + uri);
+			throw new DbException("Unable to get connection at \" + uri");
+		}
+	}
+		
+
+}// CLASS
